879fbd93f50b2bf2c44091418b3cccc27d51a4f5
Lam <nguyenthiet.lam@gmail.com>
2019-01-14 22:45:53 +0100

    init works

--- trpg.ml
+++ trpg.ml
@@ -1,10 +1,14 @@
 (* Modules *)
 open Tsdl.Sdl.Init
+open Tsdl.Sdl
 
 (* Constants *)
 let screen_width = 640
 let screen_height = 480
 
 let () =
-    let truc = Init.init in 
-    ()
+    let flag = Tsdl.Sdl.Init.everything in
+    let result = Tsdl.Sdl.init flag in 
+    match result with
+    | Ok a -> Printf.printf "ok";
+    | Error b -> Printf.printf "pas ok";
