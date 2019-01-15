28a5dac84466d4ff4bd8d51288ee1a8a8637e47b
Lam <nguyenthiet.lam@gmail.com>
2019-01-15 19:58:10 +0100

    bg

--- trpg.ml
+++ trpg.ml
@@ -1,5 +1,5 @@
 (* Modules *)
-open Tsdl.Sdl
+open Tsdl
 
 (* Constants *)
 let screen_width = 640
@@ -7,12 +7,10 @@ let screen_height = 480
 let bg_path = "./asset/image/just.bmp"
 
 
-exception WTF
-
 (* Utils function *)
 let manage_result r s =
     match r with
-    | Error ( `Msg e ) ->  Tsdl.Sdl.log s e;exit 1
+    | Error ( `Msg e ) ->  Sdl.log s e;exit 1
     | Ok a -> a
 
 exception NonePointer
@@ -24,37 +22,37 @@ let deref_option p =
 
 (* Main functions *)
 let init () =
-    let flag = Tsdl.Sdl.Init.everything in
-    let result = Tsdl.Sdl.init flag in 
+    let flag = Sdl.Init.everything in
+    let result = Sdl.init flag in 
     manage_result result "Init error : %s"
 
 let create_window title w h =
-    let flag = Tsdl.Sdl.Window.windowed in
-    let result = Tsdl.Sdl.create_window title ~w:w ~h:h flag in
+    let flag = Sdl.Window.windowed in
+    let result = Sdl.create_window title ~w:w ~h:h flag in
     manage_result result "Create window error : %s"
 
 let get_window_surface window = 
-    let result = Tsdl.Sdl.get_window_surface window in
+    let result = Sdl.get_window_surface window in
     manage_result result "Get window surface error : %s"
 
 let fill_rect surface rect color =
-    let result = Tsdl.Sdl.fill_rect surface rect color in
+    let result = Sdl.fill_rect surface rect color in
     manage_result result "Fill rect error : %s"
 
 let alloc_format format_enum = 
-    let result = Tsdl.Sdl.alloc_format format_enum in
+    let result = Sdl.alloc_format format_enum in
     manage_result result "Allocate format error : %s"
 
 let update_window_surface window =
-    let result = Tsdl.Sdl.update_window_surface window in
+    let result = Sdl.update_window_surface window in
     manage_result result "Update window surface error : %s"
 
 let load_bmp string =
-    let result = Tsdl.Sdl.load_bmp string in
+    let result = Sdl.load_bmp string in
     manage_result result "Load BMP error : %s"
 
 let blit_surface src rect_src dst rect_dst =
-    let result = Tsdl.Sdl.blit_surface src rect_dst dst rect_dst in
+    let result = Sdl.blit_surface src rect_dst dst rect_dst in
     manage_result result "Blit surface error : %s"
 
 let () =
@@ -82,11 +80,11 @@ let () =
 
      (* Safely close the program *)
     let close () =
-        Tsdl.Sdl.free_surface (deref_option surface_p);
+        Sdl.free_surface (deref_option surface_p);
         surface_p := None;
-        Tsdl.Sdl.free_surface (deref_option bg_p);
+        Sdl.free_surface (deref_option bg_p);
         bg_p := None;
-        Tsdl.Sdl.destroy_window (deref_option window_p);
+        Sdl.destroy_window (deref_option window_p);
         window_p := None
     in
 
@@ -95,9 +93,10 @@ let () =
 
     load_media ();
 
+    (* Transfer image *)
     blit_surface (deref_option bg_p) None (deref_option surface_p) None;
     update_window_surface (deref_option window_p);
 
-    Tsdl.Sdl.delay (Int32.of_int 2000);
+    Sdl.delay (Int32.of_int 2000);
 
     close ();
