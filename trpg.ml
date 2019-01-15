677e459db9bcc55fcdebfc1936bb2900b60fc77a
Lam <nguyenthiet.lam@gmail.com>
2019-01-15 22:50:50 +0100

    delete useless

--- trpg.ml
+++ trpg.ml
@@ -8,10 +8,11 @@ let bg_path = "./asset/image/just.bmp"
 
 
 (* Utils function *)
-let manage_result r s =
+(* Manage Result *)
+let manage_result r =
     match r with
-    | Error ( `Msg e ) ->  Sdl.log s e;exit 1
     | Ok a -> a
+    | Error (`Msg e) -> Sdl.log "Error : %s" e;exit 1
 
 exception NonePointer
 
@@ -20,41 +21,7 @@ let deref_option p =
     | Some x -> x
     | None -> raise NonePointer
 
-(* Main functions *)
-let init () =
-    let flag = Sdl.Init.everything in
-    let result = Sdl.init flag in 
-    manage_result result "Init error : %s"
-
-let create_window title w h =
-    let flag = Sdl.Window.windowed in
-    let result = Sdl.create_window title ~w:w ~h:h flag in
-    manage_result result "Create window error : %s"
-
-let get_window_surface window = 
-    let result = Sdl.get_window_surface window in
-    manage_result result "Get window surface error : %s"
-
-let fill_rect surface rect color =
-    let result = Sdl.fill_rect surface rect color in
-    manage_result result "Fill rect error : %s"
-
-let alloc_format format_enum = 
-    let result = Sdl.alloc_format format_enum in
-    manage_result result "Allocate format error : %s"
-
-let update_window_surface window =
-    let result = Sdl.update_window_surface window in
-    manage_result result "Update window surface error : %s"
-
-let load_bmp string =
-    let result = Sdl.load_bmp string in
-    manage_result result "Load BMP error : %s"
-
-let blit_surface src rect_src dst rect_dst =
-    let result = Sdl.blit_surface src rect_dst dst rect_dst in
-    manage_result result "Blit surface error : %s"
-
+(* Main  *)
 let () =
     (* Pointers *)
     let window_p = ref None in
@@ -68,21 +35,21 @@ let () =
     (* Initialize a window and a surface *)
     let initialization () = 
         (* Initialize SDL *)
-        init ();
+        manage_result ( Sdl.init Sdl.Init.everything );
 
         (* Open a Window *)
-        window_p := Some (create_window "TRPG" screen_width screen_height);
+        window_p := Some ( manage_result (Sdl.create_window "TRPG" ~w:screen_width ~h:screen_height Sdl.Window.windowed ));
 
         (* Get surface from Window *)
         let window = deref_option window_p in
-        surface_p := Some (get_window_surface window)
+        surface_p := Some (manage_result (Sdl.get_window_surface window))
     in
 
     let load_media () = 
-        bg_p := Some (load_bmp bg_path)
+        bg_p := Some (manage_result (Sdl.load_bmp bg_path))
     in
 
-     (* Safely close the program *)
+    (* Safely close the program *)
     let close () =
         Sdl.free_surface (deref_option surface_p);
         surface_p := None;
@@ -108,8 +75,8 @@ let () =
                 | None ->
                     ()
             );
-            blit_surface (deref_option bg_p) None (deref_option surface_p) None;
-            update_window_surface (deref_option window_p);
+            manage_result (Sdl.blit_surface (deref_option bg_p) None (deref_option surface_p) None);
+            manage_result (Sdl.update_window_surface (deref_option window_p));
             game !over
         )
         else ()
