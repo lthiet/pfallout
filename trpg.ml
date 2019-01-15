14f4c9c01fc25fd6fb44c9f24d3520ddf9487681
Lam <nguyenthiet.lam@gmail.com>
2019-01-15 19:32:43 +0100

    added image

--- trpg.ml
+++ trpg.ml
@@ -4,14 +4,25 @@ open Tsdl.Sdl
 (* Constants *)
 let screen_width = 640
 let screen_height = 480
+let bg_path = "./asset/image/just.bmp"
+
 
 exception WTF
 
+(* Utils function *)
 let manage_result r s =
     match r with
     | Error ( `Msg e ) ->  Tsdl.Sdl.log s e;exit 1
     | Ok a -> a
 
+exception NonePointer
+
+let deref_option p =
+    match !p with
+    | Some x -> x
+    | None -> raise NonePointer
+
+(* Main functions *)
 let init () =
     let flag = Tsdl.Sdl.Init.everything in
     let result = Tsdl.Sdl.init flag in 
@@ -42,40 +53,51 @@ let load_bmp string =
     let result = Tsdl.Sdl.load_bmp string in
     manage_result result "Load BMP error : %s"
 
+let blit_surface src rect_src dst rect_dst =
+    let result = Tsdl.Sdl.blit_surface src rect_dst dst rect_dst in
+    manage_result result "Blit surface error : %s"
+
 let () =
+    (* Pointers *)
+    let window_p = ref None in
+    let surface_p = ref None in
+    let bg_p = ref None in
+
+    (* Initialize a window and a surface *)
     let initialization () = 
         (* Initialize SDL *)
         init ();
 
         (* Open a Window *)
-        let window = create_window "TRPG" screen_width screen_height in
+        window_p := Some (create_window "TRPG" screen_width screen_height);
 
         (* Get surface from Window *)
-        let surface = get_window_surface window in
-        window, surface 
+        let window = deref_option window_p in
+        surface_p := Some (get_window_surface window)
     in
+
     let load_media () = 
-        let bg = 
+        bg_p := Some (load_bmp bg_path)
+    in
 
-    let window, surface = initialization () in
+     (* Safely close the program *)
+    let close () =
+        Tsdl.Sdl.free_surface (deref_option surface_p);
+        surface_p := None;
+        Tsdl.Sdl.free_surface (deref_option bg_p);
+        bg_p := None;
+        Tsdl.Sdl.destroy_window (deref_option window_p);
+        window_p := None
+    in
 
 
-    (* Get RGB for white *)
-    let format_enum = Tsdl.Sdl.get_surface_format_enum surface in
-    let pixel_format = alloc_format format_enum in
-    let color = Tsdl.Sdl.map_rgb pixel_format 255 255 255 in
+    initialization ();
 
-    (* Fill rect on the window*)
-    fill_rect surface None color;
+    load_media ();
 
-    (* Update the surface *)
-    update_window_surface window;
+    blit_surface (deref_option bg_p) None (deref_option surface_p) None;
+    update_window_surface (deref_option window_p);
 
-    (* Delay *)
     Tsdl.Sdl.delay (Int32.of_int 2000);
 
-    (* Destroy *)
-    Tsdl.Sdl.destroy_window window;
-
-    (* Quit *)
-    Tsdl.Sdl.quit ();
+    close ();
