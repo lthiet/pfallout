e57bc94b5a66d2b5cdeb9f7f2aad7b12eb8fb952
Lam <nguyenthiet.lam@gmail.com>
2019-01-30 19:22:40 +0100

    grid seperated

--- asset/src/tile.ml
+++ asset/src/tile.ml
@@ -66,69 +66,14 @@ module TileGraphics = struct
     (* Modules variables *)
     let texture_path = [|"asset/image/tiles.png"|]
     let textures = ref [||]
-    let tiles : MTile.tile list ref = ref []
-    let level_width = 16
-    let level_height = 12
     let tile_height = 80
     let tile_width = tile_height
 
-
-    exception InvalidMap
 
     (* Functions *)
     let get_screen_x t = t#get_x * tile_width
     let get_screen_y t = t#get_y * tile_height
     let get_box t = Sdl.Rect.create (get_screen_x t) (get_screen_y t) tile_width tile_height
-
-    (* Format the map file into workable data *)
-    let rec map_into_char_list inc acc =
-        try
-            (* Read the input file *)
-            let a = input_char inc in
-            (* Check if a in a number *)
-            if char_is_number a then
-                (* Read the second letter *)
-                let b = input_char inc in
-                if char_is_number b then
-                    (* Discard the space *)
-                    let c = input_char inc in
-                    if c = ' ' then
-                        map_into_char_list inc ((a,b) :: acc)
-                    else
-                        raise InvalidMap
-                else
-                    raise InvalidMap
-            else
-                map_into_char_list inc acc
-        with End_of_file ->
-            acc
-
-    (* Transform the workable data from map files into a tile list *)
-    let char_list_into_tile_list l =
-        let x = ref 0 in
-        let y = ref 0 in
-        let l =
-        List.fold_left ( fun acc (a,b) -> 
-            let a_bis = 10 * ((int_of_char a)-48) in
-            let b_bis = (int_of_char b) - 48 in
-            let tile_type_n = a_bis + b_bis in
-            let tile_type = MTile.int_to_tile_type tile_type_n in
-            let tile = new MTile.tile 0 !x !y 0 tile_type in
-
-            x := !x + 1;
-            if !x >= level_width then (
-                x := 0;
-                y := !y + 1;
-            )
-            else
-            (
-                ();
-            );
-            tile :: acc
-        ) [] l
-        in
-        List.rev l
-
 
     (* Match a tile type to a clip to get the texture from *)
     let match_tile_type_to_clip t =
@@ -152,14 +97,7 @@ module TileGraphics = struct
     (* Initializes the module *)
     let init renderer =
         (* Load the textures for tiles *)
-        textures := MTexture.load_textures renderer texture_path;
-        (* Load the map *)
-        let map = open_in "asset/map/lazy.map" in
-        (* Load the tiles *)
-        let tmp = List.rev (map_into_char_list map []) in
-        tiles := char_list_into_tile_list tmp;
-        (* Close the flux *)
-        close_in map
+        textures := MTexture.load_textures renderer texture_path
 
     (* Render a tile *)
     let render renderer tile camera =
--- asset/src/utils.ml
+++ asset/src/utils.ml
@@ -32,12 +32,6 @@ let check_key_scan ks i =
 let check_ev_type e t =
     Sdl.Event.get e Sdl.Event.typ = t
 
-let check_ev_key_repeat e =
-    Sdl.Event.get e Sdl.Event.keyboard_repeat = 0
-
-let get_scancode e =
-    Sdl.Event.get e Sdl.Event.keyboard_scancode
-
 let check_collision a b =
     let la = Sdl.Rect.x a in
     let lb = Sdl.Rect.x b in
